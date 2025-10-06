93684b7829254cc13f991bc6d0aa97598bc35e5e
Vishal Sah <vishalsha8606@gmail.com>
2025-10-06 23:35:30 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoute.js';
+import userRoutes from './routes/userRoute.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
